b38b8678088460e56bc1e2b8c4c343c2f6704b41
Mr-Apisit <ton5destiny@gmail.com>
2023-05-09 15:52:00 +0700

    noname card

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 // import 'package:hive_flutter/hive_flutter.dart';
 import './model.dart';
 import './constants.dart';
+import 'src/presentation/pages/_landing_page.dart';
 // import 'src/presentation/pages/_landing_page.dart';
 
 void main() async {
@@ -14,13 +15,14 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext cntext) {
+  Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: true,
-        title: 'Flutter Demo',
-        theme: ThemeData(colorSchemeSeed: const Color.fromARGB(255, 211, 211, 211)),
-        // home: const LandingPage(),
-        home: const Example1());
+      debugShowCheckedModeBanner: true,
+      title: 'Flutter Demo',
+      theme: ThemeData(colorSchemeSeed: const Color.fromARGB(255, 211, 211, 211)),
+      // home: const LandingPage(),
+      home: const Example1(),
+    );
   }
 }
 
@@ -29,7 +31,7 @@ class Example1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final data = List.from(travelList).map((e) => TravelModel.fromJson(e));
+    // final data = List.from(travelList).map((e) => TravelModel.fromJson(e));
 
     // final Widget image = Semantics(
     //   label: '${data.first.id} ${data.first.user}',
@@ -49,14 +51,86 @@ class Example1 extends StatelessWidget {
         child: ConstrainedBox(
           constraints: const BoxConstraints(
             minWidth: 300,
-            maxWidth: 380,
-            minHeight: 150,
-            maxHeight: 200,
+            maxWidth: 400,
+            minHeight: 650,
+            maxHeight: 950,
           ),
           child: Container(
+            height: 700,
             alignment: Alignment.center,
             decoration: const BoxDecoration(color: Color(0xFFFFFFF0)),
-            child: const Text("Demo"),
+            child: SizedBox(
+              height: 150,
+              width: 250,
+              // color: Colors.red.withOpacity(0.5),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  Positioned.fill(
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            "https://pixabay.com/get/geffeb7f1b330b6287874d7767ff016f7dcd1adf5b3078b217ea70ed8f385ab233a59beb97f08f2fe6dbf3b9bec2ba6512c9a257ec08eb2a061eb9c5996b317df_640.jpg",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 5,
+                    left: 10,
+                    child: Container(
+                      height: 30,
+                      width: 20,
+                      decoration: const BoxDecoration(
+                        // color: Colors.black,
+                        image: DecorationImage(
+                          opacity: .4,
+                          image: AssetImage("images/logo.png"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 45,
+                    decoration: BoxDecoration(
+                      color: Colors.black.withOpacity(.2),
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(4.0),
+                        bottomRight: Radius.circular(4.0),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 50,
+                    left: 10,
+                    child: Text(
+                      "Is' me Gugu",
+                      style: TextStyle(color: Colors.white.withOpacity(.7)),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 25,
+                    left: 10,
+                    child: Text(
+                      "Is' me Gugu",
+                      style: TextStyle(fontSize: 10, color: Colors.white.withOpacity(.4)),
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 5,
+                    right: 10,
+                    child: Text(
+                      "Ref.Gugu",
+                      style: TextStyle(fontSize: 6, color: Colors.white.withOpacity(.4)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
             // child: _stack(image),
           ),
         ),
