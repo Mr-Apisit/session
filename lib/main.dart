c2e39149714d2181b163793ac0d000022c3e3dab
Mr-Apisit <ton5destiny@gmail.com>
2023-05-10 16:33:52 +0700

    add list card

--- lib/main.dart
+++ lib/main.dart
@@ -73,7 +73,7 @@ class Example1 extends StatelessWidget {
                         image: DecorationImage(
                           fit: BoxFit.cover,
                           image: NetworkImage(
-                            "https://pixabay.com/get/geffeb7f1b330b6287874d7767ff016f7dcd1adf5b3078b217ea70ed8f385ab233a59beb97f08f2fe6dbf3b9bec2ba6512c9a257ec08eb2a061eb9c5996b317df_640.jpg",
+                            "https://pixabay.com/get/g54e2c3cd12dc3581b81d65262950e99d59e12f52f75be48d1f1b798d48e0a67fb3fc80626ea54015663252f12098da237f9e2865dc4984a2e752bd65458696af_640.jpg",
                           ),
                         ),
                       ),
@@ -137,17 +137,153 @@ class Example1 extends StatelessWidget {
       ),
     );
   }
+}
 
-  // Widget _stack(Widget image) {
-  //   return Stack(
-  //     alignment: Alignment.bottomCenter,
-  //     children: [
-  //       image,
-  //       const Text(
-  //         "Hello world",
-  //         style: TextStyle(color: Colors.white),
-  //       ),
-  //     ],
-  //   );
-  // }
+class ExampleListCard extends StatelessWidget {
+  const ExampleListCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final data = List.from(travelList).map((e) => TravelModel.fromJson(e));
+
+    return Material(
+      child: ListView(
+        children: [
+          const SizedBox(
+            height: 70,
+            width: double.maxFinite,
+          ),
+          SizedBox(
+            height: 200,
+            width: double.maxFinite,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                for (TravelModel item in data)
+                  CardWidget(
+                    item.webformatUrl,
+                    title: item.type,
+                    tag: item.tags,
+                    ref: item.user,
+                    isHorizontal: false,
+                  )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 3000,
+            child: ListView(
+              reverse: true,
+              physics: const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.only(top: 10),
+              scrollDirection: Axis.vertical,
+              children: [
+                for (TravelModel item in data)
+                  CardWidget(
+                    item.webformatUrl,
+                    title: item.type,
+                    tag: item.tags,
+                    ref: item.user,
+                    isHorizontal: true,
+                  )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CardWidget extends StatelessWidget {
+  final String picUrl;
+  final String title;
+  final String tag;
+  final String ref;
+  final bool isHorizontal;
+  const CardWidget(
+    this.picUrl, {
+    required this.title,
+    required this.tag,
+    required this.ref,
+    required this.isHorizontal,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 250,
+      padding: EdgeInsets.only(bottom: 15, left: isHorizontal ? 50 : 10, right: isHorizontal ? 50 : 10),
+      // color: Colors.red.withOpacity(0.5),
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(Radius.circular(4.0)),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                    picUrl,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 5,
+            left: 10,
+            child: Container(
+              height: 30,
+              width: 20,
+              decoration: const BoxDecoration(
+                // color: Colors.black,
+                image: DecorationImage(
+                  opacity: .4,
+                  image: AssetImage("images/logo.png"),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 45,
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(.7),
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(4.0),
+                bottomRight: Radius.circular(4.0),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 50,
+            left: 10,
+            child: Text(
+              title,
+              style: TextStyle(color: Colors.white.withOpacity(.9)),
+            ),
+          ),
+          Positioned(
+            bottom: 25,
+            left: 10,
+            child: Text(
+              tag,
+              style: TextStyle(fontSize: 10, color: Colors.white.withOpacity(.8)),
+            ),
+          ),
+          Positioned(
+            bottom: 5,
+            right: 10,
+            child: Text(
+              ref,
+              style: TextStyle(fontSize: 6, color: Colors.white.withOpacity(.7)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
