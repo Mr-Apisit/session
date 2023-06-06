cd29f6c3d3feef0f24b21231a362bc189a8ded55
Mr-Apisit <ton5destiny@gmail.com>
2023-06-06 15:42:33 +0700

    Login ui

--- lib/src/presentation/features/authentication/ui/authen_page.dart
+++ lib/src/presentation/features/authentication/ui/authen_page.dart
@@ -5,6 +5,55 @@ class AuthenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Login"),
+      ),
+      body: Container(
+        width: double.infinity,
+        alignment: Alignment.topCenter,
+        color: Colors.yellow,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              const Text("Login เข้าระบบ"),
+              const SizedBox(
+                width: 300,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: "username",
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              const SizedBox(
+                width: 300,
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "password",
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              OutlinedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        'กำลังเข้าสู่ระบบ....',
+                      ),
+                    ),
+                  );
+                },
+                child: const Text("เข้าสู่ระบบ"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
